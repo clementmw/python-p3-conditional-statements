46a10adb84518250fbc3839c7826c34d3bec3191
clementmwamba <clementmwamba23@gmail.com>
2023-12-07 12:18:41 +0300

    4

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -9,6 +9,7 @@ def admin_login (username,password):
 #access = admin_login("admin","1235")
 #print(access)
 
+#howistheweather
 def hows_the_weather(temperature):
    responce = (
        "brisk" if temperature < 40 else
@@ -16,10 +17,37 @@ def hows_the_weather(temperature):
        "too dang hot" if temperature > 85 else
        "perfect"
    )
-   return f"It's {responce} out here!"
+   return f"It's {responce} out there!"
     
 today = hows_the_weather(45)
 print(today)
 
+#fizzbuzz
+def fizzbuzz(num):
+    if num % 3 == 0 and num % 5 == 0 :
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num 
+    
+print(fizzbuzz(5))
 
+def calculator (operation, num1, num2):
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        return num1 / num2
+    else:
+        print ("Invalid operation!")
+        return None
+        
+    
+print(calculator("+", 3, 5))
 
